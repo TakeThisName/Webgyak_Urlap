c050713d1b553fb41c67e4b08e60de9bb0fa94a1
avaaris <avaaris@gmail.com>
2018-11-07 19:34:40 +0100

    JS_Userchk

--- JS/urlap.js
+++ JS/urlap.js
@@ -1,7 +1,27 @@
 
 function UserCheck(){
-    var userName = document.getElementById("username");
+    // 8-25 karakter, nem kezdődhet számmal + ERR üzenet
+    document.getElementById("usernameERR").innerHTML = "";
+    var userName = document.getElementById("username").value;
     userName = userName.trim();
-    console.log(UserName.length);
+    
+    if (userName.length > 7 && userName.length < 26){
+        if (isFinite(userName[0])){
+            document.getElementById("usernameERR").innerHTML = "Nem kezdődhet számmal!";
+        }
+    }
+    else {
+        document.getElementById("usernameERR").innerHTML = "8 és 25 karakter között";
+    }
+    
+    //console.log(userName.length);
 }
 
+function RegForm(){
+    form = document.getElementsByTagName("input");
+    return form;
+}
+
+function UserCheck_2(urlap){
+    console.log(urlap[0].value);
+}
