c44f149b0bfdf8cfd5b29218147e91eede891e8b
avaaris <avaaris@gmail.com>
2018-11-14 16:50:00 +0100

    Passw_chk_R

--- JS/urlap.js
+++ JS/urlap.js
@@ -17,6 +17,43 @@ function UserCheck(){
     //console.log(userName.length);
 }
 
+function PasswChk(){
+    var passw = document.getElementById("password").value;
+    var passw_b = document.getElementById("password2").value;
+    document.getElementById(("passwERR")).innerHTML = "";
+    document.getElementById(("passwERR2")).innerHTML = "";
+    var cn = 0;
+    var ck = 0;
+    var csz = 0;
+    
+    // min 8 karakter
+    if (passw.length < 8)
+        document.getElementById("passwERR").innerHTML = "Min 8 karakter!";
+    
+    // kis- nagybetű
+    for (var i = 0; i < passw.length; i++){
+        if (passw[i] === passw[i].toUpperCase())
+            cn++;
+        if (passw[i] === passw[i].toLowerCase())
+            ck++;
+        if (!isNaN(passw[i]))
+            csz++;
+    }
+    if (cn == 0)
+        document.getElementById("passwERR").innerHTML = "Nagy betűk is kellenek!";
+    if (ck == 0)
+        document.getElementById("passwERR").innerHTML = "Kis betűk is kellenek!";
+    
+    // szám
+    if (csz == 0)
+        document.getElementById("passwERR").innerHTML = "Számok és betűk is kellenek!";
+    // egyezés
+    if (passw != passw_b)
+        document.getElementById("passwERR2").innerHTML = "A jelszavak nem egyeznek!";
+    
+    
+}
+
 function RegForm(){
     form = document.getElementsByTagName("input");
     return form;
